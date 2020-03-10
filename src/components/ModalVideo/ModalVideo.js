41f64834c0695ca6e82e871f7aa50f72039fdca8
Michael Sosa <833@holbertonschool.com>
2020-03-10 11:10:49 -0500

    Decrease one user

--- src/components/ModalVideo/ModalVideo.js
+++ src/components/ModalVideo/ModalVideo.js
@@ -1,15 +1,31 @@
 import React, { useState } from "react";
 import { Modal, Row, Col } from "react-bootstrap";
 import Video from "./Video/Video";
+import axios from "axios";
 
 const ModalVideo = props => {
   const [show, setShow] = useState(true);
   const [published, setPublished] = useState(true);
-  // const [publisherVideo, setPublisherVideo] = useState(false);
 
-  // const handlePublished = value => setPublished(value);
+  /* ====================== decrease one user ============================*/
+  const decreaseUserFromRoom = async roomId => {
+    const url = `${process.env.REACT_APP_API_URL}/api/rooms/decrease/${roomId}`;
+    try {
+      await axios({
+        method: "PUT",
+        headers: {
+          token: process.env.REACT_APP_ZAFRA_KEY
+        },
+        url: url
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const handleClose = () => {
+    // Delete one user from the room
+    decreaseUserFromRoom(props.roomId);
     // Check if session is conected
     setPublished(false);
     setShow(false);
@@ -52,17 +68,6 @@ const ModalVideo = props => {
       <Row>
         <Col md={8} className="video">
           {handleOTSession()}
-          <Modal.Footer>
-            {/* <Button variant="secondary" onClick={handleVideo}>
-              Video
-            </Button>
-            <Button variant="secondary" onClick={handleShow}>
-              Audio
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Hang Up
-            </Button> */}
-          </Modal.Footer>
         </Col>
         <Col md={4} className="chat">
           <Modal.Header>

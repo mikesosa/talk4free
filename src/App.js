c3e1d843b5ac082d1dd7322871781d2e1bdf3c9b
jorgezafra94 <947@holbertonschool.com>
2020-03-11 16:48:40 -0500

    [update] exporting request in frontend

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import ChatRooms from "./components/ChatRooms/ChatRooms";
 import Jumbotron from "./components/Jumbotron/Jumbotron";
 import socketIOClient from "socket.io-client";
 // import { Container } from "react-bootstrap";
-import axios from "axios";
+// import axios from "axios";
 import "./App.scss";
+import { Users, AddUserinDb, CheckIfUser } from "./controllers/ApiRequests";
 
 class App extends React.Component {
   state = {
@@ -21,46 +22,19 @@ class App extends React.Component {
     users: []
   };
 
-  checkUser = async () => {
-    const result = await axios({
-      method: "GET",
-      headers: {
-        token: process.env.REACT_APP_ZAFRA_KEY
-      },
-      url: `${process.env.REACT_APP_API_URL}/api/users`
-    });
-    const users = result.data;
-    if (users.length > 0) {
-      for (let index = 0; index < users.length; index++) {
-        if (users[index].email === this.state.email) {
-          return true;
-        }
-      }
-    }
-    return false;
+  checkUser = async email => {
+    return CheckIfUser(email);
   };
 
-  saveUser = async () => {
-    if (this.state.email !== "") {
-      const alreadyExist = await this.checkUser();
+  saveUser = async data => {
+    if (data.email !== "") {
+      const alreadyExist = await this.checkUser(this.state.email);
       if (!alreadyExist) {
-        axios({
-          method: "POST",
-          url: `${process.env.REACT_APP_API_URL}/api/users`,
-          headers: {
-            token: process.env.REACT_APP_ZAFRA_KEY
-          },
-          data: {
-            email: this.state.email,
-            username: this.state.userName,
-            active: true,
-            adm: false,
-            img: this.state.imageUrl
-          }
-        });
+        await AddUserinDb(data);
       }
     }
   };
+
   updateLogin = res => {
     this.setState({
       isLoggedIn: res.isSignedIn,
@@ -68,26 +42,23 @@ class App extends React.Component {
       email: res.email,
       imageUrl: res.imageUrl
     });
-    this.saveUser();
+    // data to send
+    let data = {
+      email: this.state.email,
+      username: this.state.userName,
+      active: true,
+      adm: false,
+      img: this.state.imageUrl
+    };
+    this.saveUser(data);
   };
 
   getAllUsers = async () => {
-    // This will get all the users with an active room
-    await axios({
-      method: "GET",
-      headers: {
-        token: process.env.REACT_APP_ZAFRA_KEY
-      },
-      url: `${process.env.REACT_APP_API_URL}/api/users`
-    })
-      .then(res => {
-        let users = res.data.filter(user => user.room_id !== null);
-        res.data = users;
-        this.setState({
-          users: res.data
-        });
-      })
-      .catch(err => console.log(err));
+    console.log(this.state.users);
+    let response = await Users();
+    this.setState({
+      users: response
+    });
   };
 
   componentDidMount() {
@@ -102,12 +73,12 @@ class App extends React.Component {
     socket.on("closeUserresp", resp => {
       if (resp) this.getAllUsers();
     });
-
+    // real existing rooms
     socket.on("renderRoom", resp => {
       if (resp) this.getAllUsers();
     });
-    // This will get all the users with an active room
   }
+
   render() {
     return (
       <React.Fragment>
@@ -122,11 +93,6 @@ class App extends React.Component {
           email={this.state.email}
           users={this.state.users}
         />
-        {/* <footer className="mt-auto">
-          <Container>
-            <p>Hello</p>
-          </Container>
-        </footer> */}
       </React.Fragment>
     );
   }
---
+++ src/controllers/ApiRequests.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+/* =================================     get users with rooms     ===================================== */
+let Users = async () => {
+  let res = await axios({
+    method: "GET",
+    headers: {
+      token: process.env.REACT_APP_ZAFRA_KEY
+    },
+    url: `${process.env.REACT_APP_API_URL}/api/users`
+  });
+  let users = res.data.filter(user => user.room_id !== null);
+  res.data = users;
+  return res.data;
+};
+
+/* =================================     Add a user if not exist in db     ===================================== */
+
+let AddUserinDb = async data => {
+  console.log("esta es la data que llega", data);
+  await axios({
+    method: "POST",
+    url: `${process.env.REACT_APP_API_URL}/api/users`,
+    headers: {
+      token: process.env.REACT_APP_ZAFRA_KEY
+    },
+    data
+  });
+  return true;
+};
+
+/* =================================     check if user with email exists in Db     ===================================== */
+
+let CheckIfUser = async email => {
+  const result = await axios({
+    method: "GET",
+    headers: {
+      token: process.env.REACT_APP_ZAFRA_KEY
+    },
+    url: `${process.env.REACT_APP_API_URL}/api/users`
+  });
+  const users = result.data;
+  if (users.length > 0) {
+    for (let index = 0; index < users.length; index++) {
+      if (users[index].email === email) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+export { Users, AddUserinDb, CheckIfUser };
